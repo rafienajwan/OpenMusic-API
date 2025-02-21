23d2f6b3b9bd43aff521829cab27bf58ddd7e270
rafienajwan <rafie.anjasmara@gmail.com>
2025-02-21 21:35:04 +0700

    add playlists API with handlers and routes for managing playlists

---
+++ src/api/playlists/handler.js
@@ -0,0 +1,54 @@
+const autoBind = require('auto-bind');
+
+class PlaylistsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postPlaylistHandler(request, h) {
+    this._validator.validatePlaylistPayload(request.payload);
+    const { name } = request.payload;
+    const { id: owner } = request.auth.credentials;
+
+    const playlistId = await this._service.addPlaylist({ name, owner });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Playlist has been added successfully',
+      data: {
+        playlistId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getPlaylistsHandler(request) {
+    const { id: owner } = request.auth.credentials;
+    const playlists = await this._service.getPlaylists(owner);
+    return {
+      status: 'success',
+      data: {
+        playlists,
+      },
+    };
+  }
+
+  async deletePlaylistByIdHandler(request) {
+    const { playlistId } = request.params;
+    const { id: owner } = request.auth.credentials;
+
+    await this._service.verifyPlaylistOwner(playlistId, owner);
+    await this._service.deletePlaylistById(playlistId);
+
+    return {
+      status: 'success',
+      message: 'Playlist has been deleted',
+    };
+  }
+}
+
+module.exports = PlaylistsHandler;
