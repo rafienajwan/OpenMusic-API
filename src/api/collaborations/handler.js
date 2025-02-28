c87918ed1817db35faf033c43021189a5db69243
rafienajwan <rafie.anjasmara@gmail.com>
2025-03-01 06:17:40 +0700

    feat: add CollaborationsHandler and routes for managing collaborations

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,70 @@
+const autoBind = require('auto-bind');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class CollaborationsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postCollaborationHandler(request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { playlistId, userId } = request.payload;
+
+      await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+      const collaborationId = await this._collaborationsService.addCollaboration(playlistId, userId);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Successfully added collaboration',
+        data: {
+          collaborationId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(404);
+        return response;
+      }
+
+      if (error instanceof InvariantError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(400);
+        return response;
+      }
+
+      // Re-throw the error if it's not a NotFoundError or InvariantError
+      throw error;
+    }
+  }
+
+  async deleteCollaborationHandler(request) {
+    this._validator.validateCollaborationPayload(request.payload);
+    const { id: credentialId } = request.auth.credentials;
+    const { playlistId, userId } = request.payload;
+
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+    await this._collaborationsService.deleteCollaboration(playlistId, userId);
+
+    return {
+      status: 'success',
+      message: 'Successfully deleted collaboration',
+    };
+  }
+}
+
+module.exports = CollaborationsHandler;
