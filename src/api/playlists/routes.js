472c018c42caa292b830d88af27a0d6628ce4a3d
rafienajwan <rafie.anjasmara@gmail.com>
2025-02-28 17:58:04 +0700

    add authentication options for playlist and song routes

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -3,16 +3,49 @@ const routes = (handler) => [
     method: 'POST',
     path: '/playlists',
     handler: handler.postPlaylistHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/playlists',
     handler: handler.getPlaylistsHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/playlists/{playlistId}',
     handler: handler.deletePlaylistByIdHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'POST',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.postPlaylistSongHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.getPlaylistSongsHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{playlistId}/songs/{songId}',
+    handler: handler.deletePlaylistSongByIdHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   }
 ];
 
