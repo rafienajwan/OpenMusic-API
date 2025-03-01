099d56af174479872f79287eff6906eed1b14057
rafienajwan <rafie.anjasmara@gmail.com>
2025-03-01 20:40:04 +0700

    feat: enable error request logging and simplify collaborations service registration

--- src/server.js
+++ src/server.js
@@ -48,6 +48,9 @@ const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
+    debug: {
+      request:['error'],
+    },
     routes: {
       cors: {
         origin: ['*'],
@@ -112,7 +115,7 @@ const init = async () => {
     {
       plugin: collaborations,
       options: {
-        service: collaborationsService,
+        collaborationsService,
         playlistsService,
         validator: CollaborationsValidator,
       },
